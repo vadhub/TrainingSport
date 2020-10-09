b8125b90fb870411c8beaeb881e033a60367c6a5
Vadim <gabderahmanov99@gmail.com>
2020-10-09 15:17:29 +0400

    add activity Information

---
+++ app/src/main/java/com/vadim/trainingsport/Information.java
@@ -0,0 +1,22 @@
+package com.vadim.trainingsport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_information);
+    }
+
+    public void onClickToHome(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
