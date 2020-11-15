3bfdd66e571e3d564643fe507d5e2166b2e2a210
Vadim <gabderahmanov99@gmail.com>
2020-11-15 12:44:34 +0400

    update ExercisesDatabase

--- app/src/main/java/com/vad/calk/ExercisesDatabase.java
+++ app/src/main/java/com/vad/calk/ExercisesDatabase.java
@@ -1,10 +1,31 @@
 package com.vad.calk;
 
+import android.content.Context;
+
 import androidx.room.Database;
 
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = Exercise.class, version = 1, exportSchema = false)
 public abstract class ExercisesDatabase extends RoomDatabase {
+
+    private ExercisesDatabase exercisesDatabase;
+    private static final String DB_NAME = "exercises.db";
+
+    private static final Object LOCK = new Object();
+
+    public ExercisesDatabase getInstance(Context context){
+
+        synchronized (LOCK){
+            if(exercisesDatabase == null){
+                exercisesDatabase = Room.databaseBuilder(context, ExercisesDatabase.class, DB_NAME).build();
+            }
+        }
+
+        return exercisesDatabase;
+    }
+    
+    public abstract ExercieseDao exercieseDao();
 
 }
