37436beb000d2928c44ef9731e4af073ebece52e
Vadim <gabderahmanov99@gmail.com>
2020-11-15 12:37:33 +0400

    update dao

--- app/src/main/java/com/vad/calk/ExercieseDao.java
+++ app/src/main/java/com/vad/calk/ExercieseDao.java
@@ -1,4 +1,22 @@
 package com.vad.calk;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
 public interface ExercieseDao {
+
+    @Query("SELECT * FROM exercises ORDER BY date")
+    void getAllExercises();
+
+    @Insert
+    void insertExercise(Exercise exercise);
+
+    @Delete
+    void deleteExercise(Exercise exercise);
+
+    @Query("DELETE FROM exercises")
+    void deleteAllExercises();
 }
